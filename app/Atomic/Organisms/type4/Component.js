b3a9d545dd2eff8fa2fb4d702057f732e9995c10
bitaru <aiir.diir@gmail.com>
2016-08-25 18:58:29 +0300

    Export as html

---
+++ app/Atomic/Organisms/type4/Component.js
@@ -0,0 +1,49 @@
+import styles from './styles.css';
+
+const getStyles = settings => {
+  const res = {
+    backgroundColor: settings.get('backgroundColor')
+  };
+
+  const padding = settings.get('padding');
+  if (padding) {
+    const { top, right, bottom, left } = padding;
+    res.padding = `${top}px ${right} ${bottom} ${left}`;
+  }
+
+  const backgroundImage = settings.get('backgroundImage');
+  if (backgroundImage) {
+    const [url, cover, { x, y }, repeat] = backgroundImage;
+    res.background = `url(${url}) ${x} ${y} ${repeat} ${settings.get('backgroundColor')}`;
+    res.backgroundSize = cover;
+  }
+
+  return res;
+};
+
+export default ({
+  settings,
+  molecules
+}) => (
+  <div
+    className={styles.wrap}
+    style={getStyles(settings)}>
+    <div className={styles.container}>
+      <div className={styles.side}>
+        <div className={styles.rightSideContainer}>
+          {molecules.get('Main')}
+        </div>
+      </div>
+      <div className={styles.side}>
+        <div className={styles.rightSideContainer}>
+          {molecules.get('Second')}
+        </div>
+      </div>
+      <div className={styles.side}>
+        <div className={styles.rightSideContainer}>
+          {molecules.get('Third')}
+        </div>
+      </div>
+    </div>
+  </div>
+);
--- app/components/Root/index.js
+++ app/components/Root/index.js
@@ -9,6 +9,9 @@ import { Menu } from '../Menu';
 import { withEditorContext } from 'Editor/editorContext';
 import createEditor from 'Editor/creators/createEditor';
 import selector from 'modules/builder/selectors';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import styles from './styles.css';
 const Editor = createEditor({ edit: true });
@@ -16,16 +19,14 @@ const Editor = createEditor({ edit: true });
 export const Root = compose(
   connect(selector),
   withEditorContext,
-  withHandlers({
-    exportEditor: props => () => window.render(props)
-  })
+  DragDropContext(HTML5Backend), // eslint-disable-line new-cap
 )(({ blocks, exportEditor, ...props }) => {
   return (
     <div className={styles.wrap}>
       {
         props.editingItem.canEdit && <Menu />
       }
-      <Editor data={blocks} exportEditor={exportEditor} />
+      <Editor data={blocks} />
     </div>
   );
 }
