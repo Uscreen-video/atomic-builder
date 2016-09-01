c7349ac59afb2a876602e356bdbb323e7d9a6527
bitaru <aiir.diir@gmail.com>
2016-09-01 16:41:47 +0300

    Rebase

--- app/Editor/draft/index.js
+++ app/Editor/draft/index.js
@@ -1,7 +1,7 @@
-import { EditorState, Editor } from 'draft-js';
+import { EditorState, Editor, convertToRaw, convertFromRaw } from 'draft-js';
 import { stateFromHTML } from 'draft-js-import-html';
 import { compose, withState, withHandlers, shouldUpdate } from 'recompose';
-
+import { isString } from 'lodash';
 import renderOptions from 'Editor/draft/helpers/renderOptions';
 
 import Toolbar from './Toolbar';
@@ -15,16 +15,15 @@ let editor = void 0;
 
 export default compose(
   shouldUpdate(() => false),
-  withState('content', 'setContent', ({ value }) =>
-    !value
-    && EditorState.createEmpty(decorator)
-    || value instanceof EditorState
-      && value
-      || EditorState.createWithContent(stateFromHTML(value), decorator)
-  ),
+  withState('content', 'setContent', ({ value }) => {
+    if (!value) return EditorState.createEmpty(decorator);
+    if (value instanceof EditorState) return value;
+    if (isString(value)) return EditorState.createWithContent(stateFromHTML(value), decorator);
+    return EditorState.createWithContent(convertFromRaw(value.toJS ? value.toJS() : value));
+  }),
   withHandlers({
     onChange: props => state => {
-      props.setContent(state, props.onChange(state))
+      props.setContent(state, props.onChange(convertToRaw(state.getCurrentContent())));
     }
   })
 )(({ content, onChange }) => (
